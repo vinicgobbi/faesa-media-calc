ed6400b661761a0a1b4aff91c319488ea7317142
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2023-07-07 17:45:10 -0300

    Publicado no github

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿decimal[] notas = new decimal[3];
+decimal notasSomadas = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Digite sua nota:");
+    string nota = Console.ReadLine()!;
+    if (nota.Contains("."))
+    {
+        nota = nota.Replace(".", ",");
+    }
+    notas[i] = Convert.ToDecimal(nota);
+}
+
+foreach (int i in notas)
+{
+    notasSomadas += i;
+}
+
+decimal notasDivididas = (decimal) notasSomadas / 3;
+
+string status = notasDivididas > 7 ? "Aprovado" : "Reprovado";
+
+Console.WriteLine($"Você tirou {notasDivididas:F2} e ficou {status}");
---
+++ README.md
@@ -0,0 +1,7 @@
+Um simples projeto onde o aluno insere as notas e o programa retorna a média delas
+
+Obs.:
+
+- Este programa foi feito para prática de estudos em csharp
+
+- Não espere que o código esteja bem feito, afinal ainda estou aprendendo, e caso ache necessário, farei alterações
